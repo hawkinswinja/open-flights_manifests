91740e6a748bd19a470e73a2ff85af856aee8c78
hawkinswinja <hawkinswinja@gmail.com>
2024-04-30 22:39:25 +0300

    add: manifestfiles for open-flights app

---
+++ open-flights/postgres-secret.sh
@@ -0,0 +1,10 @@
+# change credentials as needed except for REPLICATION_USER
+# if you change REPLICATION_USER also change chenge the data in configmap under pg_hba.conf
+
+kubectl create secret generic postgresql \
+  --from-literal POSTGRES_USER="postgresadmin" \
+  --from-literal POSTGRES_PASSWORD='admin123' \
+  --from-literal POSTGRES_DB="postgresdb" \
+  --from-literal REPLICATION_USER="replicationuser" \
+  --from-literal REPLICATION_PASSWORD='replicationPassword' \
+  --from-literal POSTGRES_HOST='postgres-svc'
